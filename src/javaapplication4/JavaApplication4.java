95cb7310635a120d6b7847cd9dc945a863eb4cc4
1406571 <1406571@Y350SDP06.ad.dundeeandangus.ac.uk>
2015-12-04 10:37:28 +0000

    01 - start

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -21,6 +21,8 @@ public class JavaApplication4 {
         System.out.println("Hello, world!");
         System.out.println("another line");
         System.out.println("another change");
+        System.out.println("in the next commit, the line below will be deleted");
+        System.out.println("another change");
     }
     
 }
